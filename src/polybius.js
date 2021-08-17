8a5abc8a73537d952667650af075a19cd1a993ea
Beau Hale <halebeau3@gmail.com>
2021-08-17 07:08:06 -0700

    Wrote polybius.js & polybius.test.js

--- src/polybius.js
+++ src/polybius.js
@@ -4,10 +4,45 @@
 // of the anonymous function on line 6
 
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
 
   function polybius(input, encode = true) {
-    // your solution code here
+    //create our polybius square collection
+    const polybiusSquare = {
+      1: { 1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e'},
+      2: { 1: 'f', 2: 'g', 3: 'h', 4: 'i/j', 5: 'k'},
+      3: { 1: 'l', 2: 'm', 3: 'n', 4: 'o', 5: 'p'},
+      4: { 1: 'q', 2: 'r', 3: 's', 4: 't', 5: 'u'},
+      5: { 1: 'v', 2: 'w', 3: 'x', 4: 'y', 5: 'z'},
+    };
+
+    const message = input.toLowerCase().split(''); //our input -> individual char array
+    const messageNoSpaces = message.filter(nums => nums != ' '); //our array  without spaces
+    if(!encode){ //then we decode
+      let decodeString = '';
+      if(messageNoSpaces.length % 2 != 0) return false; //a copy of our array without spaces to check if there are an odd number of numbers
+      for(let index = 0; index < message.length; index += 2){
+        //[column][row] format
+        if(message[index] === ' '){ //if theres a space in the input add it to our decodeString
+          decodeString += ' ';
+          index--; //reset index a space since our value was a space
+        }else{ decodeString += polybiusSquare[message[index + 1]][message[index]]; } //decode
+      }
+      return decodeString;
+    }else{ //else we encode
+      const buildEncryption = [];
+      for(let letter of message){ //for each letter we will find the key/value pair by:
+        if(letter === ' '){ buildEncryption.push(' '); } //keep our spaces
+        for(let c = 1; c < 6; c++){ //cycling each column
+          for(let i = 1; i < 6; i++){ //cycling each row
+            if(polybiusSquare[c][i].includes(letter)){
+              buildEncryption.push(i);
+              buildEncryption.push(c);
+            } 
+          }
+        }
+      }
+      return buildEncryption.join('');
+    }
   }
 
   return {
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,35 @@
 // Write your tests here!
+const expect = require('chai').expect;
+const { polybius } = require('../src/polybius');
+
+describe('polybius', () =>{
+    it('Output is still a string when encoding', () => {
+        let actual = polybius('Hello'); //encoding a message
+        expect(actual).to.be.a('string');
+    });
+
+    it('The number of characters in the string (excluding spaces) should be even (when decoding)', () => {
+        let actual = polybius('3251131343 2543241341', false); //decoding
+        let expected = 'hello world';
+        expect(actual).to.equal(expected);
+    });
+
+    it('Spaces maintained throughout when decoding', () => {
+        let actual = polybius('3251131343 2543241341 4244 4234 2351', false); //decoding
+        let expected = 'hello world i/jt i/js me';
+        expect(actual).to.equal(expected);
+    });
+
+    it('Capital letters are ignored', () => {
+        let actual = polybius('HI THERE'); //encoding
+        let expected = '3242 4432512451';
+        expect(actual).to.equal(expected);
+    });
+
+    it('Letters "I" and "J" share a space (as i/j) and the number (42)', () => {
+        let actual = polybius('Johnny Jitters'); //encoding
+        let expected = '424332333345 42424444512434';
+        expect(actual).to.equal(expected);
+    });
+
+});

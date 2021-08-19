b8e730e33e3c18e8972b6ca90314665c6efa7fc3
Beau Hale <halebeau3@gmail.com>
2021-08-19 08:17:08 -0700

    Added: commented out caesar.js

--- src/caesar.js
+++ src/caesar.js
@@ -6,30 +6,29 @@
 const caesarModule = (function () {
   // you can add any code you want within this function scope
   function caesar(input, shift, encode = true) {
-    if (!shift || shift === 0 || shift < -25 || shift > 25) { return false };
-
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    const result = [];
-    input = input.toLowerCase().split('');  
+    if (!shift || shift === 0 || shift < -25 || shift > 25) { return false }; // if shift is outside of parameters return false
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; // create a alphabet variable
+    const result = [];  // create a result array variable
+    input = input.toLowerCase().split('');  // take input and make it lower case and split it into an array
     
-    if (encode === true) {
-      input.forEach((char, index) => {
-        if (alphabet.includes(char)) {
+    if (encode === true) { // if enocde === true 
+      input.forEach((char, index) => { 	// loop through your input
+        if (alphabet.includes(char)) { 	// make sure it is included in the alphabet variable else its a space or something
           alphabet.find((letter, index) => {
             if (char === letter) {
-              let newIndex = index + shift;
-              if( newIndex < 0) { newIndex = newIndex + 26};
+              let newIndex = index + shift; 	// find its index add the shift
+              if( newIndex < 0) { newIndex = newIndex + 26}; 	// if the shift is above or below 26 add 26 or minus 26 to get to the other end of the array
               if( newIndex > 25) { newIndex = newIndex - 26};
               newChar = alphabet[newIndex];
               result.push(newChar);
             }
           })
         } else {
-          result.push(char);
+          result.push(char); 	// push the new alphabet new index into tht result
         }
       });
     } else {
-      input.forEach((char, index) => {
+      input.forEach((char, index) => { 	// same as above but change the shift 
         if (alphabet.includes(char)) {
           alphabet.find((letter, index) => {
             if (char === letter) {
@@ -45,7 +44,7 @@ const caesarModule = (function () {
         }
       });
     }
-    return result.join('');
+    return result.join(''); //return the array joined into a string
     }
   return {
     caesar,
